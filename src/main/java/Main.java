5813e33b660a3985a5579f803fe22782fd5dbdf4
AlidzhanHyusein <bratneznam12@outlook.com>
2026-01-06 01:04:07 +0200

    implemented echo function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
+        int a = 2;
+        int b = 2;
 
         while(true){
             System.out.print("$ ");
@@ -11,6 +13,11 @@ public class Main {
 
             if(input.equals("exit")){
                 break;
+            }
+            if(input.equals("echo")){
+                input = scanner.nextLine();
+                System.out.println(input);
+                continue;
             }
 
             System.out.println(input + ": command not found");
