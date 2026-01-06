2303d300ff6d99f9b36a6d9a2babe663fdf59fef
AlidzhanHyusein <bratneznam12@outlook.com>
2026-01-07 01:32:37 +0200

    Implamented localable and executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,54 @@
-import java.util.Arrays;
+import java.io.File;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Set<String> builtins = Set.of("exit", "echo", "type");
 
-        while(true){
+        while (true) {
             System.out.print("$ ");
             System.out.flush();
 
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) continue;
 
-            String[] commands = {"exit","echo","type"};
-
-            String[] parts = input.split(" ",2);
+            String[] parts = input.split(" ", 2);
             String command = parts[0];
-            String arguments = parts.length > 1 ? parts[1] :"";
+            String arguments = parts.length > 1 ? parts[1] : "";
 
-            switch (command){
+            switch (command) {
                 case "exit" -> System.exit(0);
                 case "echo" -> System.out.println(arguments);
-                case "type" -> System.out.println(Arrays.stream(commands).anyMatch(c -> c.equals(arguments)) ? arguments + " is a shell builtin" :   arguments + ": not found");
-                default -> System.err.printf("%s: command not found%n",input);
+                case "type" -> {
+                    if(builtins.contains(arguments)) {
+                        System.out.println(arguments +" is a shell builtin");
+                        return;
+                    }
 
+                    String pathEnv = System.getenv("PATH");
+
+                    if(pathEnv == null && pathEnv.isEmpty()) {
+                        System.out.println(command + ": not found");
+                        return;
+                    }
+
+                    String[] paths = pathEnv.split(File.pathSeparator);
+
+                    for(String dir : paths){
+                        File file = new File(dir,command);
+
+                        if(file.exists() && file.isFile() && file.canExecute()){
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            return;
+                        }
+                    }
+                    System.out.println(command + ": not found");
+                }
+                default -> System.err.println(command + ": command not found");
             }
-
         }
     }
 }
